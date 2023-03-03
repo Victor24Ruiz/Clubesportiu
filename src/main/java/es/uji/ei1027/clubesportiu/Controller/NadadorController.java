724c9e8a15f9ffc68a0b2491972b2bb0db87688b
Victor24Ruiz <vsegarra02@gmail.com>
2023-03-03 12:06:53 +0100

    Add

--- src/main/java/es/uji/ei1027/clubesportiu/Controller/NadadorController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/Controller/NadadorController.java
@@ -1,10 +1,14 @@
 package es.uji.ei1027.clubesportiu.Controller;
 
 import es.uji.ei1027.clubesportiu.Dao.NadadorDao;
+import es.uji.ei1027.clubesportiu.Model.Nadador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/nadador")
@@ -20,5 +24,19 @@ public class NadadorController {
     public String listNadadors(Model model) {
         model.addAttribute("nadadors", nadadorDao.getNadadors());
         return "nadador/list";
+    }
+
+    @RequestMapping("/add")
+    public String addNadador(Model model) {
+        model.addAttribute("nadador", new Nadador());
+        return "nadador/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("nadador") Nadador nadador, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "nadador/add";
+        nadadorDao.addNadador(nadador);
+        return "redirect:list";
     }
 }
