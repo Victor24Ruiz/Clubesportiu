ce05d07e412448205245247dceef02c3eafbb144
Victor24Ruiz <vsegarra02@gmail.com>
2023-03-31 12:48:12 +0200

    Prova

--- src/main/java/es/uji/ei1027/clubesportiu/Controller/ProvaController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/Controller/ProvaController.java
@@ -1,10 +1,14 @@
 package es.uji.ei1027.clubesportiu.Controller;
 
 import es.uji.ei1027.clubesportiu.Dao.ProvaDao;
+import es.uji.ei1027.clubesportiu.Model.Prova;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/prova")
@@ -20,5 +24,19 @@ public class ProvaController {
     public String listProva(Model model) {
         model.addAttribute("proves", provaDao.getProves());
         return "prova/list";
+    }
+
+    @RequestMapping("/add")
+    public String addNadador(Model model) {
+        model.addAttribute("prova", new Prova());
+        return "prova/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("prova") Prova prova, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "prova/add";
+        provaDao.addProva(prova);
+        return "redirect:list";
     }
 }
--- src/main/java/es/uji/ei1027/clubesportiu/Dao/ProvaDao.java
+++ src/main/java/es/uji/ei1027/clubesportiu/Dao/ProvaDao.java
@@ -20,6 +20,33 @@ public class ProvaDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    public void addProva(Prova prova) {
+        jdbcTemplate.update("INSERT INTO Prova VALUES(?, ?, ?, ?)", prova.getNom(), prova.getDescripcio(),
+                prova.getTipus(), prova.getData());
+    }
+
+    public void deleteProva(Prova prova) {
+        jdbcTemplate.update("DELETE FROM Prova WHERE nom =?", prova.getNom());
+    }
+
+    public void deleteProva(String nom) {
+        jdbcTemplate.update("DELETE FROM Prova WHERE nom =?", nom);
+    }
+
+    public void updateProva(Prova prova) {
+        jdbcTemplate.update("UPDATE Prova SET descripcio =?, tipus =?, data =? WHERE nom =?", prova.getDescripcio(),
+                prova.getTipus(), prova.getData(), prova.getNom());
+    }
+
+    public Prova getProva(String nom) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM Prova WHERE nom =?", new ProvaRowMapper(), nom);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
     public List<Prova> getProves() {
         try {
             return jdbcTemplate.query("SELECT * FROM Prova", new ProvaRowMapper());
