4fe00518446df39d377e8edfcab08e6a23041f76
Victor24Ruiz <vsegarra02@gmail.com>
2023-03-31 12:53:21 +0200

    Prova Controller

--- src/main/java/es/uji/ei1027/clubesportiu/Controller/ProvaController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/Controller/ProvaController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/prova")
@@ -38,5 +42,27 @@ public class ProvaController {
             return "prova/add";
         provaDao.addProva(prova);
         return "redirect:list";
+    }
+
+    @RequestMapping(value = "/update/{nom}", method = RequestMethod.GET)
+    public String editProva(Model model, @PathVariable String nom) {
+        model.addAttribute("prova", provaDao.getProva(nom));
+        List<String> typeList = Arrays.asList("Individual", "Grupal");
+        model.addAttribute("typeList", typeList);
+        return "prova/update";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(@ModelAttribute("prova") Prova prova, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "prova/update";
+        provaDao.updateProva(prova);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/delete/{nom}")
+    public String processDelete(@PathVariable String nom) {
+        provaDao.deleteProva(nom);
+        return "redirect:../list";
     }
 }
