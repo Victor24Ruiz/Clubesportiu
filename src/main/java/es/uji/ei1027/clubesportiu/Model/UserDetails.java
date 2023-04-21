bbfa432ebff45326dfcfa216d096791a1b57400e
Victor24Ruiz <vsegarra02@gmail.com>
2023-04-21 11:46:49 +0200

    UserDetails

---
+++ src/main/java/es/uji/ei1027/clubesportiu/Model/UserDetails.java
@@ -0,0 +1,22 @@
+package es.uji.ei1027.clubesportiu.Model;
+
+public class UserDetails {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername() {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword() {
+        this.password = password;
+    }
+}
